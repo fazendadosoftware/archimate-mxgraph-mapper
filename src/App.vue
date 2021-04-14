e4e1fa5cd20f4218358221e0c5dfdd2c9e5f8ebe
Paulo dos Santos <paulo@motovalor.com>
2021-04-14 11:15:48 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Hi there! {{t}}</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      t: new Date().toISOString()
+    }
+  },
+  created () {
+    setInterval(() => {
+      this.t = new Date().toISOString()
+    }, 100)
+  }
+}
+</script>
